7718d46e3e947b5c802b73748da09141eda97418
Paula Macedo <paulamacedo@users.noreply.github.com>
2021-01-24 10:15:37 -0300

    Add files via upload

---
+++ threejs/anim_1.js
@@ -0,0 +1,34 @@
+function WaveAnimation() {}
+
+Object.assign( WaveAnimation.prototype, {
+
+    init: function() {
+        let upperArmTween = new TWEEN.Tween( {theta:0} )
+            .to( {theta:Math.PI/2}, 500)
+            .onUpdate(function(){
+                // This is an example of rotation of the right_upper_arm 
+                // Notice that the transform is M = T * R 
+                let right_upper_arm =  robot.getObjectByName("right_upper_arm");
+                right_upper_arm.matrix.makeRotationZ(this._object.theta).premultiply( new THREE.Matrix4().makeTranslation(2.6, 0, 0 ) );
+                // Updating final world matrix (with parent transforms) - mandatory
+                right_upper_arm.updateMatrixWorld(true);
+                // Updating screen
+                stats.update();
+                renderer.render(scene, camera);    
+            })
+
+        upperArmTween.start();
+    },
+    animate: function(time) {
+        window.requestAnimationFrame(this.animate.bind(this));
+        TWEEN.update(time);
+    },
+    run: function() {
+        this.init();
+        this.animate(0);
+    }
+});
+
+
+
+
