9a5bbb71b2d13ab2c014eaa17730dc05f5d75180
Paula Macedo <paulamacedo@users.noreply.github.com>
2021-01-28 23:03:47 -0300

    cabeça ok

--- threejs/anim_3.js
+++ threejs/anim_3.js
@@ -1,2 +1,35 @@
+function Gargalhar() {}
 
+Object.assign( Gargalhar.prototype, {
+
+    init: function() {
+
+        function headTween(){
+            return new TWEEN.Tween( {y:4.8} )
+                .to( {y:5.2}, 500)
+                .onUpdate(function(){
+                    let head = robot.getObjectByName('head');
+                    head.matrix.makeTranslation(0,this._object.y,0);
+                    head.updateMatrixWorld(true);
+                    stats.update();
+                    renderer.render(scene, camera);    
+                })
+        }
+       
+        let hd = headTween();
+        hd.repeat(10);
+        hd.start();
+
+  
+       
+    },
+    animate: function(time) {
+        window.requestAnimationFrame(this.animate.bind(this));
+        TWEEN.update(time);
+    },
+    run: function() {
+        this.init();
+        this.animate(0);
+    }
+});
 
